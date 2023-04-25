1dea2d4a2b673f81d1757dbecc6a93b7183f473e
mahmud035 <mahamudulhasan4148@gmail.com>
2023-04-25 17:51:11 +0600

    19 - function signature

---
+++ TypeScript-Anisul Islam/function-signature/index.js
@@ -0,0 +1,15 @@
+var userInfo1;
+var userInfo2;
+var userInfo3;
+userInfo1 = function () {
+    console.log("Anisul Islam is 32 years old");
+};
+userInfo2 = function (name) {
+    console.log("".concat(name, " is 32 years old"));
+};
+userInfo3 = function (name, age) {
+    return "".concat(name, " is ").concat(age, " years old");
+};
+userInfo1();
+userInfo2('Anisul Islam');
+userInfo3('Anisul Islam', 32);
---
+++ TypeScript-Anisul Islam/function-signature/index.ts
@@ -0,0 +1,19 @@
+let userInfo1: () => void;
+let userInfo2: (name: string) => void;
+let userInfo3: (name: string, age: number) => string;
+
+userInfo1 = () => {
+  console.log(`Anisul Islam is 32 years old`);
+};
+
+userInfo2 = (name: string) => {
+  console.log(`${name} is 32 years old`);
+};
+
+userInfo3 = (name: string, age: number) => {
+  return `${name} is ${age} years old`;
+};
+
+userInfo1();
+userInfo2('Anisul Islam');
+userInfo3('Anisul Islam', 32);
