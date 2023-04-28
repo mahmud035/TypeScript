7246ad7afb160e7dd541bddcb84ca2d9038adffd
mahmud035 <mahamudulhasan4148@gmail.com>
2023-04-28 16:19:18 +0600

    Class is typescript

--- TypeScript-Hitesh Part 02/dist/index.js
+++ TypeScript-Hitesh Part 02/dist/index.js
@@ -1,1 +1,10 @@
 "use strict";
+class User {
+    constructor(email, name) {
+        this.city = '';
+        this.email = email;
+        this.name = name;
+    }
+}
+const pavel = new User('pavel@.com', 'pavel');
+console.log(pavel);
