f90018e9ab6544f96c69a82bd06abf24b5063030
mahmud035 <mahamudulhasan4148@gmail.com>
2023-04-28 06:58:11 +0600

    first intro to typescript docs

---
+++ TypeScript-Hitesh/variableMe.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = 'Hello Pavel';
+var myNum = 10;
+console.log(greetings);
---
+++ TypeScript-Hitesh/variableMe.ts
@@ -0,0 +1,7 @@
+let greetings: string = 'Hello Pavel';
+
+let myNum = 10;
+
+console.log(greetings);
+
+export {};
