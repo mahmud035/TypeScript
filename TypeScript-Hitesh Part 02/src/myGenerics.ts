fbd09e6edc3122269131cca2d399c8e3f13736b9
mahmud035 <mahamudulhasan4148@gmail.com>
2023-04-29 10:23:18 +0600

    generic basics example - anisul islam

--- TypeScript-Hitesh Part 02/src/myGenerics.ts
+++ TypeScript-Hitesh Part 02/src/myGenerics.ts
@@ -1,3 +1,4 @@
+// Generics Basics
 const score: Array<number> = [];
 const names: Array<string> = [];
 
@@ -20,6 +21,20 @@ function identifyFour<T>(value: T): T {
   return value;
 }
 
+function printUserInfo<X, Y>(userId: X, UserAge: Y) {
+  console.log(`ID: ${userId}, Age: ${UserAge}`);
+}
+
+// with arrow function
+const printUserInfo2 = <X, Y>(userId: X, UserAge: Y) => {
+  console.log(`ID: ${userId}, Age: ${UserAge}`);
+};
+
+printUserInfo('101', 32);
+printUserInfo(101, '32');
+printUserInfo2(101, 32);
+printUserInfo2('101', '32');
+
 interface Bootle {
   brand: string;
   type: number;
@@ -33,6 +48,7 @@ function getSearchProducts<T>(products: T[]): T {
   return products[3];
 }
 
+// Generic with Arrow Function
 const getMoreSearchProducts = <T>(products: T[]): T => {
   // do some database operations
   const myIndex = 3;
