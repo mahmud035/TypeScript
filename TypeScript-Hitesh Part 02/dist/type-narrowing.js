9fafc6d996ed33fc651aba544302f30370453e49
mahmud035 <mahamudulhasan4148@gmail.com>
2023-04-29 15:10:22 +0600

    Exhaustiveness checking

--- TypeScript-Hitesh Part 02/dist/type-narrowing.js
+++ TypeScript-Hitesh Part 02/dist/type-narrowing.js
@@ -58,5 +58,19 @@ function getTrueShape(shape) {
     if (shape.kind === 'circle') {
         return Math.PI * shape.radius ** 2;
     }
-    return shape.side * shape.side;
+    // return shape.side * shape.side;
+}
+// Exhaustiveness checking
+function getArea(shape) {
+    switch (shape.kind) {
+        case 'circle':
+            return Math.PI * shape.radius ** 2;
+        case 'square':
+            return shape.side * shape.side;
+        case 'rectangle':
+            return shape.length * shape.width;
+        default:
+            const _defaultforshape = shape;
+            return _defaultforshape;
+    }
 }
